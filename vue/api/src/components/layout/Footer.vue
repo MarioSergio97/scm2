83ef4506c11b93ede11eb1d942e52b635806919a
henry1806 <henry1806@nauta.cu>
2022-02-21 20:38:27 -0500

    - footer link bugtracker

--- vue/api/src/components/layout/Footer.vue
+++ vue/api/src/components/layout/Footer.vue
@@ -1,12 +1,12 @@
 <template>
     <footer class="main-footer">
         <div class="footer-left">
-          <a href="http://localhost/mantisbt" class="text-danger" style="font-size: 13px">
-            <i class="fa fa-bug"/>
-            <a-tooltip placement="top" title="Link al BugTracker">
-              <span>Reportar un error</span>
-            </a-tooltip>
-          </a>
+<!--          <a href="http://localhost/mantisbt" class="text-danger" style="font-size: 13px">-->
+<!--            <i class="fa fa-bug"/>-->
+<!--            <a-tooltip placement="top" title="Link al BugTracker">-->
+<!--              <span>Reportar un error</span>-->
+<!--            </a-tooltip>-->
+<!--          </a>-->
         </div>
         <div class="footer-right">
         </div>
